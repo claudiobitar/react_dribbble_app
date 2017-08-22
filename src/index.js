551036ad37a26ebe13799875a32ba509b197b445
Claudio Bitar <claudiobitar@gmail.com>
2017-08-22 15:11:27 -0300

    Transfering the index dribbble content to a separated dribbble component

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Dribbble extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { work: [] };
+  }
+
+  componentDidMount() {
+    this.ShotList();
+  }
+
+  ShotList() {
+    return $.getJSON('https://api.dribbble.com/v1/shots?per_page=3&access_token=41ff524ebca5e8d0bf5d6f9f2c611c1b0d224a1975ce37579326872c1e7900b4&callback=?')
+      .then((resp) => {
+        this.setState({ work: resp.data.reverse() });
+      });
+  }
+  
+  /*
+   $('.box').click(function () {
+     $('body').append('<div id="modal' + val.id + '" class="modal fade" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><img class="avatar img-circle" src="' + val.user.avatar_url + '"/><h3>' + val.user.name + '</h3></div><div class="modal-body"><p>' + val.description + '</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>');
+   });  
+  */
+  
+  render() {
+    const works = this.state.work.map((val, i) => {
+      return <ul>
+        <li className="box" data-toggle="modal" data-target="#modal' + val.id + '">
+          <img className="cover" src={val.images.normal} />
+          <div className="bar">
+            <h2>{val.title}</h2>
+            <span>{val.views_count}'</span>
+            <i className="fa fa-eye fa-2x" aria-hidden="true"></i>
+          </div>
+        </li>
+      </ul>
+    });
+
+    return <div>{works}</div>    
+  }
+}
+
+ReactDOM.render(<Dribbble />, document.querySelector('.container'));
